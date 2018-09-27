603faacb884caa2e2d1a02fd49ce1c61f39bd96b
Brock Gibson <brockgibson@Brocks-MacBook-Pro.local>
2018-09-27 15:28:50 -0500

    Added more content to the page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,63 @@ import "./scss/app.scss";
 
 class App extends React.Component {
   render() {
-    return <div className="container">test</div>;
+    return (
+      <React.Fragment>
+        <div className="container">
+          <h1>Webpack Exploration</h1>
+          <h3>This page was deployed and compiled by webpack, react, babel</h3>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            auctor odio eget nisl tincidunt feugiat. Aenean varius augue eu
+            lacus pharetra, blandit sagittis orci dignissim. Maecenas varius
+            pretium nibh, nec congue lectus consectetur vitae. Mauris et
+            elementum urna, egestas dapibus odio. Curabitur gravida laoreet
+            cursus. Vestibulum facilisis nunc commodo dui dictum egestas.
+            Vestibulum congue lacus id orci fringilla, molestie auctor massa
+            porttitor. Morbi hendrerit nunc et tincidunt volutpat. Nullam et
+            neque quis justo consequat condimentum sed non lectus. Praesent quis
+            dapibus mauris, at blandit velit. Maecenas euismod placerat diam
+            lobortis cursus. Etiam gravida tempus porttitor. Mauris mauris
+            sapien, maximus blandit cursus ut, dictum at quam. Nulla porta id
+            tortor in laoreet. Phasellus ac egestas turpis, ut pellentesque
+            turpis. Nulla facilisi. Morbi sit amet nibh ac orci fringilla
+            sodales sed id lorem. Nulla elementum purus vitae elit pulvinar
+            luctus. Pellentesque et urna id magna condimentum egestas. Donec
+            efficitur diam est, sit amet dignissim ipsum sollicitudin at.
+            Curabitur ac diam dolor. Integer iaculis molestie mauris sit amet
+            finibus. Suspendisse ut hendrerit mi. Phasellus eu odio porttitor
+            eros venenatis cursus at eu diam. Curabitur placerat ac tellus nec
+            aliquam. Donec nec leo neque. Donec convallis laoreet pretium. Etiam
+            tristique, lectus eget rhoncus mattis, urna tortor luctus lacus,
+            eget molestie turpis erat sit amet mi. Proin rutrum vestibulum
+            fermentum. Nunc interdum ipsum in libero eleifend, consectetur
+            lobortis urna viverra. Integer commodo, metus scelerisque aliquet
+            sodales, dolor magna scelerisque nisi, vel aliquet nisi nulla
+            aliquet odio. Morbi eu mauris aliquet, fermentum nulla et, eleifend
+            dolor. Sed rhoncus, arcu nec malesuada eleifend, libero purus tempor
+            arcu, quis posuere neque massa eget nulla. Suspendisse potenti.
+            Proin sit amet odio sem. Morbi sagittis dictum elit mattis molestie.
+            Aenean mattis pellentesque dolor, quis varius velit semper ut.
+            Phasellus nec molestie tortor. Nullam eu lectus sapien. Vivamus
+            pretium non velit in ornare. Sed tempor vestibulum finibus.
+            Pellentesque at bibendum arcu. Quisque accumsan nisi eu eros
+            sollicitudin vehicula. Fusce laoreet, ligula quis consequat egestas,
+            quam tortor mattis felis, vel semper ex quam et augue. Vestibulum
+            rutrum tristique ipsum aliquam gravida. Donec et euismod elit. Fusce
+            sodales a magna sit amet euismod. Integer iaculis diam quam, et
+            rhoncus felis laoreet vel. Aliquam ultrices dignissim nibh pulvinar
+            maximus. Maecenas rutrum leo a porta luctus. Fusce laoreet nulla at
+            leo cursus, eget commodo eros gravida. Pellentesque quis odio
+            vehicula, molestie dui et, dapibus nunc. Proin sed quam volutpat,
+            porta eros eget, venenatis ligula. Nunc tincidunt rhoncus libero, ac
+            facilisis leo pulvinar quis. Donec nibh justo, sodales sit amet
+            mattis sed, vehicula a dui. Proin egestas malesuada nunc sit amet
+            pulvinar.
+          </p>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 export default App;
